fe3687d4cecae9c9af823ad5efc66c469d4a0fa5
Will Bryant <will.bryant@gmail.com>
2009-03-20 17:09:08 +1300

    updated the test database setup to work with Rails 2.3

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,7 +17,7 @@ FileUtils.mkdir File.join(File.dirname(__FILE__), "log") rescue nil
 RAILS_DEFAULT_LOGGER = ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "log", "#{ENV['RAILS_ENV']}.log"))
 
 ActiveRecord::Base.configurations = YAML::load(IO.read(File.join(File.dirname(__FILE__), "database.yml")))
-ActiveRecord::Base.establish_connection
+ActiveRecord::Base.establish_connection ActiveRecord::Base.configurations[ENV['RAILS_ENV']]
 ActiveSupport::TestCase.send(:include, ActiveRecord::TestFixtures) if ActiveRecord.const_defined?('TestFixtures')
 ActiveSupport::TestCase.fixture_path = File.join(File.dirname(__FILE__), "fixtures")
 
