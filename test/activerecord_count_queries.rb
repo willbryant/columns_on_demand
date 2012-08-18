7e1a3fa811e34ff9150dd479c7768b46c328c965
Will Bryant <will.bryant@gmail.com>
2012-08-18 16:01:58 +1200

    pull in SQLCounter for rails 3.2

--- test/activerecord_count_queries.rb
+++ test/activerecord_count_queries.rb
@@ -11,7 +11,7 @@ if ActiveRecord::VERSION::MAJOR < 3 || (ActiveRecord::VERSION::MAJOR == 3 && Act
 
     alias_method_chain :execute, :query_record
   end
-else
+elsif ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR < 2
   # this is from 3.1's test suite.  ugly.
   class ActiveRecord::SQLCounter
     cattr_accessor :ignored_sql
@@ -36,5 +36,35 @@ else
       end
     end
   end
+  ActiveSupport::Notifications.subscribe('sql.active_record', ActiveRecord::SQLCounter.new)
+else
+  # this is from 3.2's test suite.  ugly.
+  class ActiveRecord::SQLCounter
+    cattr_accessor :ignored_sql
+    self.ignored_sql = [/^PRAGMA (?!(table_info))/, /^SELECT currval/, /^SELECT CAST/, /^SELECT @@IDENTITY/, /^SELECT @@ROWCOUNT/, /^SAVEPOINT/, /^ROLLBACK TO SAVEPOINT/, /^RELEASE SAVEPOINT/, /^SHOW max_identifier_length/, /^BEGIN/, /^COMMIT/]
+
+    # FIXME: this needs to be refactored so specific database can add their own
+    # ignored SQL.  This ignored SQL is for Oracle.
+    ignored_sql.concat [/^select .*nextval/i, /^SAVEPOINT/, /^ROLLBACK TO/, /^\s*select .* from all_triggers/im]
+
+    cattr_accessor :log
+    self.log = []
+
+    attr_reader :ignore
+
+    def initialize(ignore = self.class.ignored_sql)
+      @ignore   = ignore
+    end
+
+    def call(name, start, finish, message_id, values)
+      sql = values[:sql]
+
+      # FIXME: this seems bad. we should probably have a better way to indicate
+      # the query was cached
+      return if 'CACHE' == values[:name] || ignore.any? { |x| x =~ sql }
+      self.class.log << sql
+    end
+  end
+
   ActiveSupport::Notifications.subscribe('sql.active_record', ActiveRecord::SQLCounter.new)
 end
